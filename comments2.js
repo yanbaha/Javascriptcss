86ea17475da6b15b2072a7c45bf3b3cae97316c2
yanbaha <yanbaha@gmail.com>
2016-11-30 23:14:36 +0000

    Rename comments.js to comments2.js

---
+++ comments2.js
@@ -0,0 +1,5 @@
+var mydiv = document.getElementById("myDiv");
+var aTag = document.createElement('a');
+aTag.setAttribute('href',"http://www.folfoly.com/");
+aTag.innerHTML = "فولفولي - استفد من الإنترنت";
+mydiv.appendChild(aTag);
