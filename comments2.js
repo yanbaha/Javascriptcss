2644f9bb0eb39c9d27f68db0a7f15bb871d0370e
yanbaha <yanbaha@gmail.com>
2016-11-30 23:24:23 +0000

    Update comments2.js

--- comments2.js
+++ comments2.js
@@ -1,5 +1,5 @@
 var mydiv = document.getElementById("myDiv");
 var aTag = document.createElement('a');
-aTag.setAttribute('href',"http://www.folfoly.com/");
+aTag.setAttribute('href',"http://www.fffdsfdfgdgrdfg.com/");
 aTag.innerHTML = "فول";
 mydiv.appendChild(aTag);
